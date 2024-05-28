5f0aa2c81d4700f8afda4eab8b9a15f43bffd0aa
pooruser123 <germanuser88@tutanota.com>
2024-05-28 10:59:39 +0200

    tset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import Button from '@mui/material/Button';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: "Encrypto",
+  description: "Secure datatransfer",
+  icons: {
+      icon: "/img/logo.ico"
+  }
+};
 
 function page() {
   return <></>
