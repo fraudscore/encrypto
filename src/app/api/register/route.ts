0e8d540f0aa7f408f1e24b1d58e055936c3dc729
pooruser123 <germanuser88@tutanota.com>
2024-05-29 11:05:32 +0200

    isvalid

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,25 +3,27 @@ import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
 
-
 const prisma = new PrismaClient();
 
 const algorithm = 'aes-256-cbc';
-const key = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
+const key = process.env.ENCRYPTION_KEY; 
+const iv = crypto.randomBytes(16); 
 
-function encrypt(text: string): string {
-    const cipher = crypto.createCipheriv(algorithm, key, iv);
+function encrypt(text: string, key: string, iv: Buffer): string {
+    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, 'base64'), iv);
     let encrypted = cipher.update(text, 'utf8', 'hex');
     encrypted += cipher.final('hex');
     return `${iv.toString('hex')}:${encrypted}`;
 }
 
-function decrypt(text: string): string {
+function decrypt(text: string, key: string): string {
     const textParts = text.split(':');
-    const iv = Buffer.from(textParts.shift() as string, 'hex');
-    const encryptedText = Buffer.from(textParts.join(':'), 'hex');
-    const decipher = crypto.createDecipheriv(algorithm, key, iv);
+    if (textParts.length !== 2) {
+        throw new Error('Invalid encrypted text format');
+    }
+    const iv = Buffer.from(textParts[0], 'hex');
+    const encryptedText = Buffer.from(textParts[1], 'hex');
+    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key, 'base64'), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);
     return decrypted.toString('utf8');
@@ -44,6 +46,19 @@ function isValidEmail(email: string): boolean {
     return emailRegex.test(email);
 }
 
+function isValidIPAddress(ipAddress: string): boolean {
+    const ipv4Pattern = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/;
+
+    const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}([0-9a-fA-F]{1,4})$/;
+
+    if (ipv4Pattern.test(ipAddress) || ipv6Pattern.test(ipAddress)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
 export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
@@ -51,9 +66,12 @@ export async function POST(req: NextRequest) {
         if(!isValidEmail(email)){
             return NextResponse.json({"error": "Not a valid email provided"})
         }
+        if(!isValidIPAddress(ip_address)){
+            return NextResponse.json({"error": "Invalid Ip Address"})
+        }
 
         const sessiontoken = generateUUID();
-        const encrypted_sessiontoken = encrypt(sessiontoken)
+        const encrypted_sessiontoken = encrypt(sessiontoken, key, iv);
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
